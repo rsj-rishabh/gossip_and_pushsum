46b1bd5cf95fccff0d012a611aa61d82f838130a
Rishabh Jaiswal <32019969+rsj-rishabh@users.noreply.github.com>
2022-01-03 20:28:15 -0500

    Add files via upload

--- Program.fsx
+++ Program.fsx
@@ -19,12 +19,11 @@ type SimulatorState = {
 
 // State of node actor
 type NodeState = {
-    Neighbors: Map<IActorRef,int>
+    Neighbors: List<IActorRef>
     RumorFrequency: int
     S: float
     W: float
     SimulatorRef: IActorRef
-    Active: bool
 }
 
 
@@ -36,9 +35,6 @@ type Message =
     | InitiatePushSum
     | ReportNeighbors of List<IActorRef>
     | AllRumorsReceived
-    | Ack
-    | Activate
-    | Deactivate
 
 
 // Declaration of constants
@@ -51,44 +47,6 @@ let selectRandom(list: List<IActorRef>) =
     testSeq |> Seq.sortBy (fun _ -> rnd.Next()) |> Seq.head
 
 
-// Function to select a random element from map
-let selectRandomFromMap(mp: Map<IActorRef, int>) =
-    let keys = mp |> Map.toSeq |> Seq.map fst
-    let rnd = Random()
-    keys |> Seq.sortBy (fun _ -> rnd.Next()) |> Seq.head
-
-
-// Function to increment Ack count for a node
-let incrAck(mp:Map<IActorRef, int>, nbrRef:IActorRef) =
-    mp 
-        |> Map.change nbrRef (fun x -> 
-            match x with
-            | Some v -> 
-                if (v+1)=10 then
-                    //printfn "[NODE] %s is considered as failed." nbrRef.Path.Name
-                    Some (-1)
-                else
-                    Some (v+1)
-            | None -> None )
-
-           
-// Function to decrement Ack count for a node
-let decrAck(mp:Map<IActorRef, int>, nbrRef:IActorRef) =
-    mp 
-        |> Map.change nbrRef (fun x -> 
-            match x with
-            | Some v -> Some (v-1)
-            | None -> None )
-
-
-// Function to get element from a Map
-let getVal(mp:Map<IActorRef, int>, nbrRef:IActorRef) =
-    let found = mp.TryFind nbrRef 
-    match found with
-    | Some v -> v
-    | None -> -2
-
-
 // Node Actor Definition
 let node (i: int) (mailbox:Actor<_>) =
     let rec loop state = actor {
@@ -96,29 +54,14 @@ let node (i: int) (mailbox:Actor<_>) =
         let sender = mailbox.Sender()
 
         match message with
-        | Activate ->
-            return! loop ({state with Active = true})
-        | Deactivate ->
-            return! loop ({state with Active = false})
-        | Ack ->
-            if getVal(state.Neighbors, sender) = -1 then
-                //printfn "[NODE] %s is considered as alive again." sender.Path.Name
-                return! loop ({state with Neighbors =  incrAck(state.Neighbors, sender)})    
-            return! loop ({state with Neighbors =  decrAck(state.Neighbors, sender)})
         | Rumor ->
-            if (sender <> state.SimulatorRef) && (sender <> mailbox.Self) then
-                sender <! Ack
-
             if state.RumorFrequency + 1 = 10 then
                 state.SimulatorRef <! AllRumorsReceived
             
             if state.RumorFrequency + 1 < 10 then
-                let mutable destination = selectRandomFromMap(state.Neighbors)
-                while getVal(state.Neighbors,destination) = -1 do
-                    destination <- selectRandomFromMap(state.Neighbors)
+                let destination = selectRandom(state.Neighbors)
                 destination <! Rumor
                 mailbox.Self <! Rumor
-                return! loop ({state with RumorFrequency = state.RumorFrequency + 1; Neighbors =  incrAck(state.Neighbors, destination)})
 
             if mailbox.Self.Path.Name = sender.Path.Name then
                 return! loop state
@@ -126,38 +69,22 @@ let node (i: int) (mailbox:Actor<_>) =
             return! loop ({state with RumorFrequency = state.RumorFrequency + 1})
         
         | InitiateGossip ->
-            if (sender <> state.SimulatorRef) && (sender <> mailbox.Self) then
-                sender <! Ack
-
-            let mutable destination = selectRandomFromMap(state.Neighbors)
-            while getVal(state.Neighbors,destination) = -1 do
-                destination <- selectRandomFromMap(state.Neighbors)
+            let destination = selectRandom(state.Neighbors)
             destination <! Rumor
             mailbox.Self <! Rumor
 
-            return! loop ({state with RumorFrequency = state.RumorFrequency + 1; Neighbors =  incrAck(state.Neighbors, destination)})
+            return! loop ({state with RumorFrequency = state.RumorFrequency + 1})
         
         | ReportNeighbors (nbrs) ->
-            let mutable nbrsMap:Map<IActorRef,int> = Map.empty
-            for nbr in nbrs do
-                nbrsMap <- nbrsMap.Add(nbr, 0)
-            return! loop ({state with Neighbors = nbrsMap; SimulatorRef = sender})
+            return! loop ({state with Neighbors = nbrs; SimulatorRef = sender})
 
         | InitiatePushSum ->
-            if (sender <> state.SimulatorRef) && (sender <> mailbox.Self) then
-                sender <! Ack
-
-            let mutable destination = selectRandomFromMap(state.Neighbors)
-            while getVal(state.Neighbors,destination) = -1 do
-                destination <- selectRandomFromMap(state.Neighbors)
+            let destination = selectRandom(state.Neighbors)
             destination <! ComputePushSum((state.S/2.0), (state.W/2.0))
             mailbox.Self <! InitiatePushSum
-            return! loop {state with S=(state.S/2.0); W=(state.W/2.0); Neighbors =  incrAck(state.Neighbors, destination)}
+            return! loop {state with S=(state.S/2.0); W=(state.W/2.0)}
         
         | ComputePushSum(s,w) ->
-            if (sender <> state.SimulatorRef) && (sender <> mailbox.Self) then
-                sender <! Ack
-
             let newS= state.S + s
             let newW = state.W + w
             let ratioDiff = state.S/state.W - newS/newW |> abs
@@ -175,21 +102,19 @@ let node (i: int) (mailbox:Actor<_>) =
                 if rf = 3 then
                     state.SimulatorRef <! AllRumorsReceived
                 
-                let mutable destination = selectRandomFromMap(state.Neighbors)
-                while getVal(state.Neighbors,destination) = -1 do
-                    destination <- selectRandomFromMap(state.Neighbors)
+                let destination = selectRandom(state.Neighbors)
                 destination <! ComputePushSum((newS/2.0), (newW/2.0))
                 mailbox.Self <! InitiatePushSum
-                return! loop {state with S=(newS/2.0); W=(newW/2.0); RumorFrequency=rf; Neighbors =  incrAck(state.Neighbors, destination)}
+                return! loop {state with S=(newS/2.0); W=(newW/2.0); RumorFrequency=rf}
                 
         | _ ->  failwith "[LOG] Unknown message."
     }
 
-    loop {Neighbors=Map.empty; RumorFrequency=0; SimulatorRef=null; S=float(i); W=1.0; Active=true}
+    loop {Neighbors=List.empty; RumorFrequency=0; SimulatorRef=null; S=float(i); W=1.0}
 
 
 // Simulator Actor Definition
-let simulator (numNodes: int, top: string, algo: string, numFails: int, systemRef: ActorSystem) (mailbox:Actor<_>) =
+let simulator (numNodes: int, top: string, algo: string, systemRef: ActorSystem) (mailbox:Actor<_>) =
     // spawn all nodes
     let nodeList = [for i in 1..numNodes do (spawn systemRef ("Node"+string(i)) (node(i)))]
     let mutable nbrs:List<IActorRef> = List.empty
@@ -257,17 +182,6 @@ let simulator (numNodes: int, top: string, algo: string, numFails: int, systemRe
         printfn "[SIMULATOR][MAIN] Initiating Gossip Protocol with %d nodes in %s topology..." numNodes top
         timer.Start()
         initNode <! InitiateGossip
-        let mutable failedSet: Set<IActorRef> = Set.empty
-        let mutable i = 0
-        while i<>numFails do
-            let failNode = selectRandom(nodeList)
-            if not (failedSet.Contains failNode) then
-                //printfn "[SIMULATOR][INFO] Deactivating %s" failNode.Path.Name
-                failNode <! Deactivate
-                failedSet <- failedSet.Add failNode
-                i <- i+1
-            else
-                i <- i-1
     else if algo = "pushsum" then
         printfn "[SIMULATOR][MAIN] Initiating Push-Sum Protocol with %d nodes in %s topology..." numNodes top
         timer.Start()
@@ -291,14 +205,14 @@ let simulator (numNodes: int, top: string, algo: string, numFails: int, systemRe
         | _ ->  failwith "[SIMULATOR][ERROR] Unknown message."
     }
 
-    loop {NumOfNodes=(numNodes-numFails); Topology=top; Algorithm=algo}
+    loop {NumOfNodes=numNodes; Topology=top; Algorithm=algo}
 
 
 // Start of the program
 let start (args: string[]) =
     let systemRef = ActorSystem.Create("System")
 
-    spawn systemRef "simulator" (simulator(int(args.[1]), string(args.[2]), string(args.[3]), int(args.[4]), systemRef)) 
+    spawn systemRef "simulator" (simulator(int(args.[1]), string(args.[2]), string(args.[3]), systemRef)) 
     |> ignore
 
     systemRef.WhenTerminated.Wait()
--- README.md
+++ README.md
@@ -1,2 +1,76 @@
-# gossip_and_pushsum
-Implementing gossip and pushsum algorithms using actor model.
+## Team Members
+- Rishabh Jaiswal (rishabh.jaiswal@ufl.edu)
+- Siddhesh Patil (siddheshpatil@ufl.edu)
+
+
+## What is working?
+We are able to simulate all topologies, algorithms, including **bonus** part.
+
+### Explanation
+
+- We are spawning two kinds of actors:
+  - <ins>Simulator:</ins> Only responsible for creating topology, randomly selecting a node to start gossip/push-sum and terminating system if all nodes have converged. **Simulator doesn't supervise Node actors.** Node actors are truely decentralized, independent, and require no supervision. 
+  - <ins>Node:</ins> Node actors have list of neighbors provided by Simulator, S/W values (in case of pushsum). We are not maintaining global variables, all nodes are independently responsible for maintaining their own states.
+- <ins>Gossip:</ins>
+
+   1. Simulator asks one random node to start gossip.
+   2. Node continuously propogates gossip to the neighbors, while continuously listening for messages.
+   3. Once, a node receives gossip 10 times, it declares itself as converged and stops sending gossips. Even if converged, node is alive to just listen to messages without performing any action.
+   4. Node informs simulator once it is converged, Simulator only keeps tracks of number of nodes in the network which are not yet converged. Once the count becomes 0, Simulator terminates the system.
+- <ins>PushSum:</ins>
+    1. Each node initially has a pair of (s,w) where s is their node number and w is 1.
+    2. Simulator asks one random node to start the pushsum.
+    3. As soon as a node receives a "start" message it continously sends (s/2,w/2) to one of its neighbors, therefore giving them a "start" signal as well. Also, node updates its (s,w) to (s/2,w/2).
+    4. On receving a pushsum, node adds it to its own (s,w) pair and sends new sum (s_new/2, w_new/2) to another neighbor.
+    5. Once the ratio of s/w stops changing more than 10x-10 for 3 consequtive times, the node declares itself as converged.
+    6. Simulator only keeps tracks of number of nodes in the network which are not yet converged. Once the count becomes 0, Simulator terminates the system.
+
+- <ins>Bonus:</ins>
+    1. In order to handle temporary or permanent failures, we are keeping track of ACK messages. Failure model as an parameter is controlled by number of lost ACK messages the node will tolerate before declaring the neighbor as inactive/failed/dead. 
+    2. Everytime a node sends a message to it's neighbor, it increments ACK count for that particular neighbor. Neighbor sends an ACK message back to the node, and the node decrements the ACK count. 
+    3. If this ACK count keeps on piling up (like, more than 5), node considers it's neighbor as dead, and makes ACK count -1.
+    4. If this neighbor returns back alive (temporary failure) and sends a message back to this node, the node resets the ACK count to 0, signifying neighbor is now alive.
+
+### How to run
+Command:
+`dotnet fsi Program.fsx <num_of_nodes> <topology> <algorithm>`
+
+Topology: `full`, `line`, `3D`, `imp3D`
+
+Algorithm: `gossip`, `pushsum`
+
+Usage:
+`dotnet fsi Program.fsx 100 "imp3D" "gossip"`
+
+### Screenshots:
+
+Gossip - Imp3D upto 1,000 nodes readings
+![alt text](img/img1.png "Gossip - Imp3D upto 1,000 nodes readings")
+
+
+Pushsum - 3D upto 1,000 nodes readings
+![alt text](img/img2.png "Pushsum - 3D upto 1,000 nodes readings")
+
+
+Gossip - 5,000 nodes Imp3D reading
+![alt text](img/img3.png "Gossip - 5,000 nodes Imp3D reading")
+
+
+Pushsum - 10,000 nodes 3D reading
+![alt text](img/img4.png "Pushsum - 10,000 nodes 3D reading")
+
+
+## What is the largest network you managed to deal with for each type of topology and algorithm?
+| Algorithm      | Topology | Nodes | Time (milliseconds)
+| ----------- | ----------- | ------| ------------- |
+| Gossip | Full | 1000  | 2,240 |
+| Gossip | Line |750 |116,375 | 
+| Gossip | 3D | 1000 | 1,443|
+| Gossip | Imp3D | 10,000 | 331,023 |
+| Pushsum | Full | 200 |157,868 |
+| Pushsum | Line |1000 | 22,956| 
+| Pushsum | 3D |10,000 | 977,305 |
+| Pushsum | Imp3D | 1000|8,908 |
+
+For Push-Sum, we managed to deal with maximum of 10,000 nodes in a 3D topology with convergence time as 977,305 ms.
+For Gossip, we managed to deal with maximum of 5,000 nodes in a imp3D topology with convergence time as 331,023 ms.
